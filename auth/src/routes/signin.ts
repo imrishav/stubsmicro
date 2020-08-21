30ee30be7ac4dde881ef5adf2075569152b48be4
Rishav Sinha <rishav@live.co.uk>
2020-08-22 01:32:33 +0530

    added Env, jwt, cookie

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,22 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation.error';
 
 const router = express.Router();
 
-router.post('/api/users/sigin', (req, res) => {
-  res.send('HI tere');
-});
+router.post(
+  '/api/users/sigin',
+  [
+    body('email').isEmail().withMessage('Email Must be valid'),
+    body('password').trim().notEmpty().withMessage('Password is required..'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+  }
+);
 
 export { router as signinRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation.error';
 import { BadRequestError } from '../errors/bad-request.error';
@@ -32,6 +33,21 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    //Geenrate web token..
+
+    const userJWT = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY!
+    );
+
+    // req.session.jwt = userJWT // this will give error so we create a new Objet
+
+    req.session = {
+      jwt: userJWT,
+    };
+
+    console.log(req.session);
 
     res.status(201).send(user);
   }
